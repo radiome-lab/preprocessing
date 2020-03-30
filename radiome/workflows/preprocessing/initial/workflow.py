4bbe03265daed53de14373b380732b02f218ed0f
Pu Zhao <puzhao@utexas.edu>
2020-03-30 13:28:31 -0500

    rename nodes

--- radiome/workflows/preprocessing/initial/workflow.py
+++ radiome/workflows/preprocessing/initial/workflow.py
@@ -10,25 +10,25 @@ def create_workflow(config: AttrDict, resource_pool: ResourcePool, context: Cont
         anatomical_image = rp[R('T1w')]
         anat_deoblique = NipypeJob(
             interface=afni.Refit(deoblique=True),
-            reference='deoblique'
+            reference='anat_deoblique'
         )
         anat_deoblique.in_file = anatomical_image
         output_node = anat_deoblique.out_file
 
         if config.non_local_means_filtering:
-            denoise = NipypeJob(interface=ants.DenoiseImage(), reference='denoise')
+            denoise = NipypeJob(interface=ants.DenoiseImage(), reference='anat_denoise')
             denoise.input_image = output_node
             output_node = denoise.output_image
 
         if config.n4_bias_field_correction:
             n4 = NipypeJob(interface=ants.N4BiasFieldCorrection(dimension=3, shrink_factor=2, copy_header=True),
-                           reference='correction')
+                           reference='anat_n4')
             n4.input_image = output_node
             output_node = n4.output_image
 
         anat_reorient = NipypeJob(
             interface=afni.Resample(orientation='RPI', outputtype='NIFTI_GZ'),
-            reference='reorient'
+            reference='anat_reorient'
         )
         anat_reorient.in_file = output_node
         rp[R('T1w', label='initial')] = anat_reorient.out_file
